fa4c045918c36982bff29d1e54d5cdca8a8a4778
Rozhan <rakrami@uwaterloo.ca>
2025-02-10 22:45:01 -0500

    Landing Pagr With Navigation

--- client/app/src/main/java/com/cs446/petpal/MainActivity.kt
+++ client/app/src/main/java/com/cs446/petpal/MainActivity.kt
@@ -3,18 +3,24 @@ package com.cs446.petpal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.cs446.petpal.ui.theme.PetPalTheme
+import com.cs446.petpal.views.LandingPageScreen
 import com.cs446.petpal.views.NewSignUpView
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             PetPalTheme {
-                NewSignUpView()
+                NavHost(navController = navController, startDestination = "landing") {
+                    composable("landing") { LandingPageScreen(navController) } // ✅ Pass `navController`
+                    composable("signup") { NewSignUpView() }
+                    composable("login") {}
+                }
             }
         }
     }
